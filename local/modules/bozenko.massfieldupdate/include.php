70c11198d6ef7e234289e1e61cab8f5cb5b0814e
varSerega <bozhenko15rpus@gmail.com>
2026-09-06 12:26:14 +0300

    Register module autoload classes globally

--- local/modules/bozenko.massfieldupdate/include.php
+++ local/modules/bozenko.massfieldupdate/include.php
@@ -1,7 +1,7 @@
 <?php
 
 if (class_exists('Bitrix\\Main\\Loader')) {
-    \Bitrix\Main\Loader::registerAutoLoadClasses('bozenko.massfieldupdate', [
+    \Bitrix\Main\Loader::registerAutoLoadClasses(null, [
         'Bozenko\MassFieldUpdate\FieldRegistry' => __DIR__.'/lib/fieldregistry.php',
         'Bozenko\MassFieldUpdate\Permission' => __DIR__.'/lib/permission.php',
         'Bozenko\MassFieldUpdate\InputParser' => __DIR__.'/lib/inputparser.php',
