8b78823eaeb1493766ec5ab0720d037aee9f011e
varSerega <bozhenko15rpus@gmail.com>
2026-09-06 12:28:33 +0300

    Load module classes directly

--- local/modules/bozenko.massfieldupdate/include.php
+++ local/modules/bozenko.massfieldupdate/include.php
@@ -1,10 +1,6 @@
 <?php
 
-if (class_exists('Bitrix\\Main\\Loader')) {
-    \Bitrix\Main\Loader::registerAutoLoadClasses(null, [
-        'Bozenko\MassFieldUpdate\FieldRegistry' => __DIR__.'/lib/fieldregistry.php',
-        'Bozenko\MassFieldUpdate\Permission' => __DIR__.'/lib/permission.php',
-        'Bozenko\MassFieldUpdate\InputParser' => __DIR__.'/lib/inputparser.php',
-        'Bozenko\MassFieldUpdate\MassUpdateService' => __DIR__.'/lib/massupdateservice.php',
-    ]);
-}
+require_once __DIR__.'/lib/fieldregistry.php';
+require_once __DIR__.'/lib/permission.php';
+require_once __DIR__.'/lib/inputparser.php';
+require_once __DIR__.'/lib/massupdateservice.php';
