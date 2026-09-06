c320380926e9947aeb4d61c1790414071f86d93b
varSerega <bozhenko15rpus@gmail.com>
2026-09-06 12:24:24 +0300

    Use absolute module autoload paths

--- local/modules/bozenko.massfieldupdate/include.php
+++ local/modules/bozenko.massfieldupdate/include.php
@@ -2,9 +2,9 @@
 
 if (class_exists('Bitrix\\Main\\Loader')) {
     \Bitrix\Main\Loader::registerAutoLoadClasses('bozenko.massfieldupdate', [
-    'Bozenko\MassFieldUpdate\FieldRegistry' => 'lib/fieldregistry.php',
-    'Bozenko\MassFieldUpdate\Permission' => 'lib/permission.php',
-    'Bozenko\MassFieldUpdate\InputParser' => 'lib/inputparser.php',
-    'Bozenko\MassFieldUpdate\MassUpdateService' => 'lib/massupdateservice.php',
+        'Bozenko\MassFieldUpdate\FieldRegistry' => __DIR__.'/lib/fieldregistry.php',
+        'Bozenko\MassFieldUpdate\Permission' => __DIR__.'/lib/permission.php',
+        'Bozenko\MassFieldUpdate\InputParser' => __DIR__.'/lib/inputparser.php',
+        'Bozenko\MassFieldUpdate\MassUpdateService' => __DIR__.'/lib/massupdateservice.php',
     ]);
 }
