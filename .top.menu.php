c8cb7c57a53086d74a30fc62e553fd53efb3f911
varSerega <bozhenko15rpus@gmail.com>
2026-09-07 21:36:12 +0300

    Add mass field update link to portal left menu

--- .top.menu.php
+++ .top.menu.php
@@ -6,6 +6,13 @@ $aMenuLinks = Array(
 		Array(), 
 		Array(), 
 		"CBXFeatures::IsFeatureEnabled('crm') && CModule::IncludeModule('crm') && CCrmPerms::IsAccessEnabled()" 
+	),
+	Array(
+		"Массовое изменение полей", 
+		"/local/massfieldupdate/index.php", 
+		Array(), 
+		Array(), 
+		"is_file(\$_SERVER['DOCUMENT_ROOT'].'/local/massfieldupdate/index.php')" 
 	)
 );
 ?>
